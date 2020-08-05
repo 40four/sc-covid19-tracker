97dd792e56f99fa3c852db300ed2c33162b70223
Sean Thompson <seanthompson44@protonmail.com>
2020-08-05 11:53:31 -0400

    Add cumulative tests chart

---
+++ scripts/runAllParsers.js
@@ -0,0 +1,6 @@
+require('./cumulativeCases');
+require('./cumulativeDeaths');
+require('./cumulativeTests');
+require('./dailyDeaths');
+require('./dailyNewCases');
+require('./dailyTestsCompleted');
