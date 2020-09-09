0049779ff213c25fd44d8131accf178ca939ff5e
Sean Thompson <seanthompson44@protonmail.com>
2020-09-09 15:31:04 -0400

    Data update, 8/7

--- scripts/cumulativeTests.js
+++ scripts/cumulativeTests.js
@@ -4,8 +4,8 @@ const dataDescription = 'cumulativeTestsCompleted';
 
 const regExCases = [
 		//RegExp('(?<=Laboratory has conducted )([0-9]+,[0-9]+|[0-9]+)'),
-		RegExp('(?<=a total of )([0-9]+,[0-9]+|[0-9]+)'),
-		RegExp('(?<=A total of )([0-9]+,[0-9]+|[0-9]+)'),
+		RegExp('(?<=a total of )([0-9]+,[0-9]+,[0-9]+|[0-9]+,[0-9]+|[0-9]+)'),
+		RegExp('(?<=A total of )([0-9]+,[0-9]+,[0-9]+|[0-9]+,[0-9]+|[0-9]+)'),
 		//RegExp('(?<=for yesterday, )([0-9]+,[0-9]+|[0-9]+)(?=\\), then multiplied)'),
 ];
 
